97b91c3f81a6233b0cc64da7aab9a3e62bce5d03
Marc Miller <marcm@google.com>
2021-01-09 23:49:29 -0500

    csv

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-console.log("Hello world");
 
+const fs = require('fs').promises;
+const express = require('express');
+const csvparse = require('csv-parse/lib/sync');
+
+let counties;
+
+(async function() {
+    const content = await fs.readFile('../covid-19-data/us-counties.csv');
+    counties = csvparse(content);
+    console.log('parsing complete');
+})();
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.send(JSON.stringify(counties));
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
