e550fa90cc5986e5c69c6ff302c81c82fe1894bb
Marc Miller <marcmill@gmail.com>
2021-01-10 01:47:16 -0500

    try using NeDB to speed up loading

--- server.js
+++ server.js
@@ -1,15 +1,57 @@
 
-const fs = require('fs').promises;
+const fs = require('fs');
 const express = require('express');
-const csvparse = require('csv-parse/lib/sync');
+const csvparse = require('csv-parser');
+const DataStore = require('nedb');
 
-let counties;
+const COUNTIES_CSV_PATH = '../covid-19-data/us-counties.csv';
+const COUNTIES_DB_PATH = 'db/us-counties.db';
 
-(async function() {
-    const content = await fs.readFile('../covid-19-data/us-counties.csv');
-    counties = csvparse(content);
-    console.log('parsing complete');
-})();
+let parseCsv = (path, cb) => {
+    let res = [];
+    fs.createReadStream(path)
+        .pipe(csvparse())
+        .on('error', (err) => { throw err; })
+        .on('data', (data) => res.push(data))
+        .on('end', () => {
+            cb(res);
+        });
+};
+
+let countiesdb;
+
+let refreshDb = (csv_path, db_path) => {
+    console.log("Refreshing counties db...");
+    parseCsv(csv_path, (res) => {
+        console.log("  ...parse complete...");
+        countiesdb = new DataStore(db_path);
+        countiesdb.loadDatabase();
+        countiesdb.ensureIndex({ fieldName: 'fips' }, (err) => {
+            if (err) throw err;
+            countiesdb.insert(res, (err, done) => {
+                if (err) throw err;
+                console.log(` ...done reading ${res.length} records.`);
+            });
+        });
+    });
+};
+
+if (!fs.existsSync(COUNTIES_DB_PATH)) {
+    refreshDb(COUNTIES_CSV_PATH, COUNTIES_DB_PATH);
+}
+else {
+    console.log('1');
+    countiesdb = new DataStore({ filename: COUNTIES_DB_PATH });
+    console.log('2');
+    countiesdb.loadDatabase();
+    console.log('3');
+    countiesdb.findOne({fips : '36091'}, (err, doc) => {
+        console.log(err, doc);
+        countiesdb.findOne({fips : '36091'}, (err, doc) => {
+            console.log(err, doc);
+        });
+    });
+}
 
 const app = express();
 
