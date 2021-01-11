d3b448134e28e4a44dba76674cc760d728abb619
Marc Miller <marcmill@gmail.com>
2021-01-11 17:18:11 -0500

    Remove db

--- server.js
+++ server.js
@@ -2,10 +2,8 @@
 const fs = require('fs');
 const express = require('express');
 const csvparse = require('csv-parser');
-const DataStore = require('nedb');
 
 const COUNTIES_CSV_PATH = '../covid-19-data/us-counties.csv';
-const COUNTIES_DB_PATH = 'db/us-counties.db';
 
 let parseCsv = (path, cb) => {
     let res = [];
@@ -18,47 +16,20 @@ let parseCsv = (path, cb) => {
         });
 };
 
-let countiesdb;
-
-let refreshDb = (csv_path, db_path) => {
-    console.log("Refreshing counties db...");
-    parseCsv(csv_path, (res) => {
-        console.log("  ...parse complete...");
-        countiesdb = new DataStore(db_path);
-        countiesdb.loadDatabase();
-        countiesdb.ensureIndex({ fieldName: 'fips' }, (err) => {
-            if (err) throw err;
-            countiesdb.insert(res, (err, done) => {
-                if (err) throw err;
-                console.log(` ...done reading ${res.length} records.`);
-            });
-        });
-    });
-};
-
-if (!fs.existsSync(COUNTIES_DB_PATH)) {
-    refreshDb(COUNTIES_CSV_PATH, COUNTIES_DB_PATH);
-}
-else {
-    console.log('1');
-    countiesdb = new DataStore({ filename: COUNTIES_DB_PATH });
-    console.log('2');
-    countiesdb.loadDatabase();
-    console.log('3');
-    countiesdb.findOne({fips : '36091'}, (err, doc) => {
-        console.log(err, doc);
-        countiesdb.findOne({fips : '36091'}, (err, doc) => {
-            console.log(err, doc);
-        });
-    });
-}
+let counties;
+parseCsv(COUNTIES_CSV_PATH, (res) => {
+    console.log(`...parse of ${ res.length } entries complete.`);
+    counties = res;
+});
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.send(JSON.stringify(counties));
+
+app.get('/county/:fips(\\d+)', (req, res) => {
+    res.send(counties.filter( i => i.fips == req.params.fips));
 });
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
+const port = Number(process.env.PORT || 3000);
+app.listen(port, () => {
+    console.log(`Listening on port ${ port }`);
 });
