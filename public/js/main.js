6136eba5c847050a137ca61e5696bdc81f657365
Marc Miller <marcmill@gmail.com>
2021-01-12 16:59:01 -0500

    more knockout

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,43 @@
 
-function addActive(data) {
-    let prev = 0;
-    let active15 = 0;
-    for (let i = 0; i < data.length; ++i) {
-        let cases = Number(data[i].cases);
-        let newc = cases - prev;
-        data[i].newc = newc;
-        active15 += newc;
-        if (i >= 15) {
-            active15 -= data[i-15].newc;
+// Calculate active cases for all window-sizes of "active": 10-day days
+class ActiveCalc {
+    constructor () {
+        this.active = new Array(11);  // tracking 10-20 days = 11 elts
+        this.active.fill(0);
+    }
+    advance(data, i) {
+        data[i].active = new Array(11);
+        for (let j = 0; j < 11; ++j) {
+            this.active[j] += data[i].newc;
+            if (i >= j+10) {
+                this.active[j] -= data[i-(j+10)].newc;
+            }
+            data[i].active[j] = this.active[j];
         }
-        data[i].active15 = active15;
-        prev = cases;
+    }
+    static add(data) {
+        let prev = 0;
+        let a = new ActiveCalc();
+        for (let i = 0; i < data.length; ++i) {
+            let cases = Number(data[i].cases);
+            let newc = cases - prev;
+            data[i].newc = newc;
+            a.advance(data, i);
+            prev = cases;
+        }
     }
 }
-
 
 $( document ).ready( () => {
     $.ajax({
         url: '/fips/36091',
         dataType: 'json',
         success: (res) => {
-            addActive(res);
-            let vm = { counties: res };
-            ko.applyBindings(vm, $('#moo').get(0));
+            ActiveCalc.add(res);
+            let viewModel = {
+                latest: res[res.length - 1]
+            };
+            ko.applyBindings(viewModel); //, $('#moo').get(0));
         },
         error: (jqxhr, tStatus, tError) => {
             alert("Ajax error.");
