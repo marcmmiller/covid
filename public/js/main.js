eaf6fce14849ef00f9f9a2bdf3cf00c15cb48890
Marc Miller <marcmill@gmail.com>
2021-01-14 00:42:01 -0500

    moar metrics

--- public/js/main.js
+++ public/js/main.js
@@ -16,14 +16,22 @@ class ActiveCalc {
         }
     }
     static add(data) {
+        let last7 = 0;
         let prev = 0;
         let a = new ActiveCalc();
         for (let i = 0; i < data.length; ++i) {
             let cases = Number(data[i].cases);
             let newc = cases - prev;
             data[i].newc = newc;
-            a.advance(data, i);
             prev = cases;
+
+            a.advance(data, i);
+
+            last7 += newc;
+            if (i >= 7)  {
+                last7 -= data[i - 7].newc;
+            }
+            data[i].last7 = last7;
         }
     }
 }
@@ -40,14 +48,26 @@ function ajdl(url, cb) {
     });
 }
 
+const g_underreportingFactor = 4;
+
 $( document ).ready( () => {
-    ajdl('/pop19/36091', (pop) => {
+    ajdl('/pop19/36091', (popres) => {
         ajdl('/fips/36091', (res) => {
             ActiveCalc.add(res);
+            let pop = Number(popres.pop);
+            let latest = res[res.length - 1];
+            let avg7 = latest.last7 / 7;
+            let avg7Per100K = avg7 / (pop / 100000);
+            let estActive = latest.active[10] * g_underreportingFactor;
+            let estActivePer100K = estActive / (pop / 100000);
             let viewModel = {
-                name: pop.name,
-                pop: pop.pop,
-                latest: res[res.length - 1]
+                name: popres.name,
+                pop: popres.pop,
+                latest: latest,
+                avg7: avg7.toFixed(2),
+                avg7Per100K: avg7Per100K.toFixed(2),
+                estActive: estActive,
+                estActivePer100K: estActivePer100K.toFixed(2)
             };
             ko.applyBindings(viewModel); //, $('#moo').get(0));
         });
